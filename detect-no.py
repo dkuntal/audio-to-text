71f97709806d7cb6635728f8ad3dbc55953a2e1c
Kuntal Das <dkuntal9@gmail.com>
2017-06-16 23:42:22 +0530

    Full file completed

--- detect-no.py
+++ detect-no.py
@@ -14,3 +14,12 @@ net = tflearn.input_data([None, 20, 80])
 net = tflearn.lstm(net, 128, dropout=0.8)
 net = tflearn.fully_connected(net, 10, activitation='softmax')
 net = tflearn.regression(net, optimizer='adam', learning_rate=learning_rate, loss='categorical_crossentropy')
+
+model = tflearn.DNN(net, tensorboard_verbose=0)
+while 1:
+	model.fit(trainX, trainY, n_epoch=10, validation_set=(testX, testY), show_metric=True,
+		batch_size=64)
+	_y = model.predict(X)
+model.save('tflearn.lstm.model')
+print(_y)
+print(y)
